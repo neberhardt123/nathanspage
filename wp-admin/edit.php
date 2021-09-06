63334fd82baf20c038002b231e86a5d675de3bb3
neberhardt123 <42151168+neberhardt123@users.noreply.github.com>
2021-09-06 15:00:35 -0700

    rtertert

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=http://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "http://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1";
+			window.location = "https://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="http://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1">http://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1">https://nathaneberhardt.com/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fstaticsite%2Fwp-admin%2Fedit.php&reauth=1</a></p>
 	</body>
 </html>
--- wp-login.php
+++ wp-login.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
-	<html lang="en-US">
+<html lang="en-US">
 	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Log In &lsaquo; Nathan Eberhardt &#8212; WordPress</title>
-	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='http://nathaneberhardt.com/wp-includes/css/dashicons.min.css?ver=5.5.3' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='http://nathaneberhardt.com/wp-includes/css/buttons.min.css?ver=5.5.3' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='http://nathaneberhardt.com/wp-admin/css/forms.min.css?ver=5.5.3' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='http://nathaneberhardt.com/wp-admin/css/l10n.min.css?ver=5.5.3' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='http://nathaneberhardt.com/wp-admin/css/login.min.css?ver=5.5.3' type='text/css' media='all' />
-	<meta name='robots' content='noindex,noarchive' />
-	<meta name='referrer' content='strict-origin-when-cross-origin' />
-		<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="http://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-32x32.jpg" sizes="32x32" />
-<link rel="icon" href="http://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-192x192.jpg" sizes="192x192" />
-<link rel="apple-touch-icon" href="http://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-180x180.jpg" />
-<meta name="msapplication-TileImage" content="http://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-270x270.jpg" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Log In &lsaquo; wutup  niggas &#8212; WordPress</title>
+	<link rel="dns-prefetch" href="//s.w.org">
+<link rel="stylesheet" id="dashicons-css" href="https://nathaneberhardt.com/wp-includes/css/dashicons.min.css?ver=5.5.5" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://nathaneberhardt.com/wp-includes/css/buttons.min.css?ver=5.5.5" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://nathaneberhardt.com/wp-admin/css/forms.min.css?ver=5.5.5" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://nathaneberhardt.com/wp-admin/css/l10n.min.css?ver=5.5.5" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://nathaneberhardt.com/wp-admin/css/login.min.css?ver=5.5.5" type="text/css" media="all">
+	<meta name="robots" content="noindex,noarchive">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="https://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-32x32.jpg" sizes="32x32">
+<link rel="icon" href="https://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-192x192.jpg" sizes="192x192">
+<link rel="apple-touch-icon" href="https://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-180x180.jpg">
+<meta name="msapplication-TileImage" content="https://nathaneberhardt.com/wp-content/uploads/2020/09/cropped-item-270x270.jpg">
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
 	<script type="text/javascript">
@@ -24,80 +24,79 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="http://nathaneberhardt.com/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://nathaneberhardt.com/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
-				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
 			</p>
 
 			<div class="user-pass-wrap">
 				<label for="user_pass">Password</label>
 				<div class="wp-pwd">
-					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20">
 					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-									<input type="hidden" name="redirect_to" value="http://nathaneberhardt.com/wp-admin/" />
-									<input type="hidden" name="testcookie" value="1" />
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="https://nathaneberhardt.com/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="http://nathaneberhardt.com/wp-login.php?action=lostpassword">Lost your password?</a>
+								<a href="https://nathaneberhardt.com/wp-login.php?action=lostpassword">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="http://nathaneberhardt.com/">
-		&larr; Back to Nathan Eberhardt		</a></p>
+				<p id="backtoblog"><a href="https://nathaneberhardt.com/">
+		&larr; Back to wutup  niggas		</a></p>
 			</div>
-	<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp' id='jquery-core-js'></script>
-<script type='text/javascript' id='zxcvbn-async-js-extra'>
+	<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp" id="jquery-core-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"http:\/\/nathaneberhardt.com\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/nathaneberhardt.com\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/zxcvbn-async.min.js?ver=1.0' id='zxcvbn-async-js'></script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4' id='wp-polyfill-js'></script>
-<script type='text/javascript' id='wp-polyfill-js-after'>
-( 'fetch' in window ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"></scr' + 'ipt>' );( document.contains ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"></scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"></scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"></scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="http://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"></scr' + 'ipt>' );
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=7.4.4" id="wp-polyfill-js"></script>
+<script type="text/javascript" id="wp-polyfill-js-after">
+( 'fetch' in window ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-fetch.min.js?ver=3.0.0"><\/scr' + 'ipt>' );( document.contains ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-node-contains.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.DOMRect ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-dom-rect.min.js?ver=3.42.0"><\/scr' + 'ipt>' );( window.URL && window.URL.prototype && window.URLSearchParams ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-url.min.js?ver=3.6.4"><\/scr' + 'ipt>' );( window.FormData && window.FormData.prototype.keys ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-formdata.min.js?ver=3.0.12"><\/scr' + 'ipt>' );( Element.prototype.matches && Element.prototype.closest ) || document.write( '<script src="https://nathaneberhardt.com/wp-includes/js/dist/vendor/wp-polyfill-element-closest.min.js?ver=2.0.2"><\/scr' + 'ipt>' );
 </script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/dist/i18n.min.js?ver=bb7c3c45d012206bfcd73d6a31f84d9e' id='wp-i18n-js'></script>
-<script type='text/javascript' id='password-strength-meter-js-extra'>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/dist/i18n.min.js?ver=772b1b30d38ec9ba77ae8ae1a39b232a" id="wp-i18n-js"></script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
 /* ]]> */
 </script>
-<script type='text/javascript' id='password-strength-meter-js-translations'>
+<script type="text/javascript" id="password-strength-meter-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-admin/js/password-strength-meter.min.js?ver=5.5.3' id='password-strength-meter-js'></script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/underscore.min.js?ver=1.8.3' id='underscore-js'></script>
-<script type='text/javascript' id='wp-util-js-extra'>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-admin/js/password-strength-meter.min.js?ver=5.5.5" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/staticsite\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-includes/js/wp-util.min.js?ver=5.5.3' id='wp-util-js'></script>
-<script type='text/javascript' id='user-profile-js-translations'>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-includes/js/wp-util.min.js?ver=5.5.5" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-translations">
 ( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type='text/javascript' src='http://nathaneberhardt.com/wp-admin/js/user-profile.min.js?ver=5.5.3' id='user-profile-js'></script>
+<script type="text/javascript" src="https://nathaneberhardt.com/wp-admin/js/user-profile.min.js?ver=5.5.5" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
-	
